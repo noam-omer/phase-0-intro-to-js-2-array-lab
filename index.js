aeb75bca70991778255ac5c955730f4744169a89
Noam Fischhoff <nfischhoff@gmail.com>
2022-07-04 12:10:33 -0400

    commit tree 7d7f821d36ee798ade63f7a2d1be746a5032ca4f

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(name){
+    const catsCopy = [...cats, name];
+    return catsCopy;
+}
+function prependCat(name){
+    const catsCopy = [name, ...cats];
+    return catsCopy;
+}
+function removeLastCat(){
+    const catsCopy = [...cats]
+    catsCopy.pop();
+    return catsCopy;
+
+}
+function removeFirstCat(){
+    const catsCopy = [...cats]
+    catsCopy.shift();
+    return catsCopy;
+}
